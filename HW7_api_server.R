909b4a2127c46141eef0d84f4b6d052b426eae13
ppandiri <prajaypandiri@gmail.com>
2025-12-02 19:24:20 -0600

    finished first draft of hw7

--- HW7_api_server.R
+++ HW7_api_server.R
@@ -1,1 +1,32 @@
-## Write your API endpoints in this file
+## HW7_api_server.R
+
+library(plumber)
+library(tidyverse)
+
+train <- read.csv("train_dataset.csv.gz")
+
+train <- train %>%
+  mutate(days_wait = as.numeric(difftime(appt_time, appt_made, units = "days")))
+
+model <- glm(
+  no_show ~ days_wait + address + specialty + provider_id + age,
+  data = train,
+  family = binomial(link = "logit")
+)
+
+#* @serializer contentType list(type="application/octet-stream")
+#* @post /predict_prob
+function(req){
+  df <- unserialize(req$body)
+  probs <- predict(model, newdata = df, type = "response")
+  serialize(as.numeric(probs), NULL)
+}
+
+#* @serializer contentType list(type="application/octet-stream")
+#* @post /predict_class
+function(req){
+  df <- unserialize(req$body)
+  p <- predict(model, newdata = df, type = "response")
+  classes <- as.integer(ifelse(p > 0.5, 1L, 0L))
+  serialize(classes, NULL)
+}
--- HW7_client_test.R
+++ HW7_client_test.R
@@ -1,1 +1,40 @@
 ## Write your client test code here
+## HW7_client_test.R
+
+library(httr)
+library(tidyverse)
+library(lubridate)
+
+base <- "http://127.0.0.1:8000"
+
+test <- read.csv("test_dataset.csv.gz")
+
+test <- test %>%
+  mutate(days_wait = as.numeric(difftime(appt_time, appt_made, units = "days")))
+
+cols <- c("days_wait", "address", "specialty", "provider_id", "age")
+newdata <- as.data.frame(test[1:5, cols])
+
+raw_input <- serialize(newdata, NULL)
+
+# ---- predict_prob ----
+resp_prob <- POST(
+  url = paste0(base, "/predict_prob"),
+  body = raw_input,
+  add_headers("Content-Type" = "application/octet-stream")
+)
+
+prob_vec <- unserialize(content(resp_prob, "raw"))
+cat("Probabilities:\n")
+print(prob_vec)
+
+# ---- predict_class ----
+resp_class <- POST(
+  url = paste0(base, "/predict_class"),
+  body = raw_input,
+  add_headers("Content-Type" = "application/octet-stream")
+)
+
+class_vec <- unserialize(content(resp_class, "raw"))
+cat("\nClasses:\n")
+print(class_vec)
